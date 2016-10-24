9174fb8ae57584e11d81c1e9cc035602b69b2091
Xavier <musxav@gmail.com>
2016-10-24 22:40:55 +0200

    bd 2

---
+++ Oprog/WNUsuari.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Oprog
+{
+    /// <summary>
+    /// Lógica de interacción para WNUsuari.xaml
+    /// </summary>
+    public partial class WNUsuari : Window
+    {
+        public WNUsuari()
+        {
+            InitializeComponent();
+        }
+    }
+}
